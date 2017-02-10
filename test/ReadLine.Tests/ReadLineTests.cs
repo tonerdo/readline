31e11c7da966d084d586d313d2bfae8cb97a8763
Toni Solarin-Sodara <toni.edward@outlook.com>
2017-02-10 12:34:44 +0100

    Add ReadLine unit tests

---
+++ test/ReadLine.Tests/ReadLineTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+using Xunit;
+
+namespace Tests
+{
+    public class Tests : IDisposable
+    {
+        public Tests()
+        {
+            string[] history = new string[] { "ls -a", "dotnet run", "git init" };
+            ReadLine.AddHistory(history);
+        }
+
+        [Fact]
+        public void TestNoInitialHistory() 
+        {
+            Assert.Equal(3, ReadLine.GetHistory().Count);
+        }
+
+        [Fact]
+        public void TestUpdatesHistory() 
+        {
+            ReadLine.AddHistory("mkdir");
+            Assert.Equal(4, ReadLine.GetHistory().Count);
+        }
+
+        [Fact]
+        public void TestGetCorrectHistory() 
+        {
+            var history = ReadLine.GetHistory();
+
+            Assert.Equal(3, history.Count);
+            Assert.Equal("git init", history.Last());
+        }
+
+        public void Dispose()
+        {
+            // If all above tests pass
+            // clear history works
+            ReadLine.ClearHistory();
+        }
+    }
+}
