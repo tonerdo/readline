a985a429dff5f0a89483b58f3fd638560f428822
Toni Solarin-Sodara <toni.edward@outlook.com>
2017-02-10 12:08:23 +0100

    Add demo program

---
+++ src/ReadLine.Demo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("ReadLine Library Demo");
+            Console.WriteLine("---------------------");
+            Console.WriteLine();
+
+            string[] history = new string[] { "ls -a", "dotnet run", "git init" };
+            ReadLine.AddHistory(history);
+
+            ReadLine.AutoCompletionHandler = (t, s) =>
+            {
+                if (t.StartsWith("git"))
+                    return new string[] { "init", "clone", "pull", "push" };
+                else
+                    return null;
+            };
+
+            string input = ReadLine.Read("(prompt)> ");
+            Console.Write(input);
+        }
+    }
+}
