e9b6637bc673473386c0731a4d81dedacac303d4
Toni Solarin-Sodara <toni.edward@outlook.com>
2017-02-16 00:28:47 +0100

    Add System.Console abstraction

---
+++ src/ReadLine/Abstractions/Console2.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace Internal.ReadLine.Abstractions
+{
+    internal class Console2 : IConsole
+    {
+        public int CursorLeft => Console.CursorLeft;
+
+        public int CursorTop => Console.CursorTop;
+
+        public int BufferWidth => Console.BufferWidth;
+
+        public int BufferHeight => Console.BufferHeight;
+
+        public void SetBufferSize(int width, int height) => Console.SetBufferSize(width, height);
+
+        public void SetCursorPosition(int left, int top) => Console.SetCursorPosition(left, top);
+
+        public void Write(string value) => Console.Write(value);
+
+        public void WriteLine(string value) => Console.WriteLine(value);
+    }
+}
