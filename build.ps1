91cd5ec05473a913f81da2aa8523b1135518d65d
Oluwatoni Solarin-Sodara <oluwatoni.sodara@ithaka.org>
2017-03-04 17:01:54 +0100

    Update powershell build script

--- build.ps1
+++ build.ps1
@@ -6,3 +6,5 @@ dotnet restore
 dotnet build ".\src\ReadLine" -c $p1
 dotnet build ".\src\ReadLine.Demo" -c $p1
 dotnet build ".\test\ReadLine.Tests" -c $p1
+
+dotnet pack ".\src\ReadLine" -o ".\artifacts" -c Release --no-build
