dea870d6f9ef187267bd5f835bc2db28c5193cc7
Toni Solarin-Sodara <toni.edward@outlook.com>
2018-01-10 13:36:28 -0700

    update csproj, bump version number

--- src/ReadLine/Properties/AssemblyInfo.cs
+++ src/ReadLine/Properties/AssemblyInfo.cs
@@ -1,6 +1,1 @@
-[assembly:System.Reflection.AssemblyTitleAttribute("ReadLine")]
-[assembly:System.Reflection.AssemblyDescriptionAttribute("A GNU-Readline like library for .NET/.NET Core")]
-[assembly:System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly:System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
-[assembly:System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly:System.Runtime.CompilerServices.InternalsVisibleTo("ReadLine.Tests")]
